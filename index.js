7992dd00b3eadcb141ba24c2e63b76c1f5610ad2
Rodrigo Mohr <rodrigom@getkor.io>
2021-04-12 16:21:14 -0600

    17

--- index.js
+++ index.js
@@ -1,5 +1,5 @@
 exports.handler = async (event) => {
-    // TODO implement for testings 16
+    // TODO implement for testings 17
     const response = {
         statusCode: 200,
         body: JSON.stringify('Hello from Git Oauth!'),
