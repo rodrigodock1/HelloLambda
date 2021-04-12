6129a8026a2092a9e55469b636fff40ff53a3703
Rodrigo Mohr <rodrigom@getkor.io>
2021-04-12 10:36:04 -0600

    tesgint pull hookds

--- index.js
+++ index.js
@@ -1,5 +1,5 @@
 exports.handler = async (event) => {
-    // TODO implement
+    // TODO implement for testings 1
     const response = {
         statusCode: 200,
         body: JSON.stringify('Hello from Git Oauth!'),
