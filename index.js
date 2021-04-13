0315066ac42f5b06b3fd7ad423f6b79fb96d56c5
Rodrigo Mohr <rodrigom@getkor.io>
2021-04-13 16:26:26 -0600

    condition agregation

--- index.js
+++ index.js
@@ -1,5 +1,5 @@
 exports.handler = async (event) => {
-    // TODO implement for testings 29
+    // TODO implement for testings 30
     const response = {
         statusCode: 200,
         body: JSON.stringify('Hello from Git Oauth!'),
