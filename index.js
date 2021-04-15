50bbd6194859816ecb5fd210ff4158aaa9ea15c8
Rodrigo Mohr <rodrigom@getkor.io>
2021-04-15 12:36:39 -0600

    sent parmas

--- index.js
+++ index.js
@@ -1,5 +1,5 @@
 exports.handler = async (event) => {
-    // TODO implement for testings 36
+    // TODO implement for testings 37
     const response = {
         statusCode: 200,
         body: JSON.stringify('Hello from Git Oauth!'),
