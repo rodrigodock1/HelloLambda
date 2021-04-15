e09479adf3357e53f07f55bc6744d4032dfe5a78
Rodrigo Mohr <rodrigom@getkor.io>
2021-04-15 13:45:02 -0600

    correct githubpull

--- index.js
+++ index.js
@@ -1,5 +1,5 @@
 exports.handler = async (event) => {
-    // TODO implement for testings 37
+    // TODO implement for testings 38
     const response = {
         statusCode: 200,
         body: JSON.stringify('Hello from Git Oauth!'),
